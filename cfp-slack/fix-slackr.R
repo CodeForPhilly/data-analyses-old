cdd6396742065be2318010c87d749e4a8ef274f2
Michael Chow <machow@princeton.edu>
2020-04-20 21:24:31 -0400

    add functions for dumping slack to postgres

--- cfp-slack/fix-slackr.R
+++ cfp-slack/fix-slackr.R
@@ -15,13 +15,16 @@ slackr_fixed_users <- function (api_token = Sys.getenv("SLACK_API_TOKEN"), curso
     json <- jsonlite::fromJSON(httr::content(tmp, as = "text"))
     members <- json$members
     cols <- setdiff(colnames(members), c("profile", "real_name"))
-    cols_prof <- grep("image_.*", names(members$profile), value = TRUE)
+    #cols_prof <- grep("image_.*", names(members$profile), value = TRUE)
     
-    users <- cbind.data.frame(members[, cols], members$profile[,cols_prof], stringsAsFactors = FALSE)
+    users <- cbind.data.frame(members[, cols], members$profile, stringsAsFactors = FALSE)
     
     cursor <- json$response_metadata$next_cursor
     if (cursor != "") {
-      users <- rbind(users, slackr_fixed_users(cursor = cursor))
+      next_users <- slackr_fixed_users(cursor = cursor)
+      # use bind_rows, since sometimes columns not returned by slack API (e.g. guest_invited_by)
+      # this ensures missing columns will be filled NA
+      users <- bind_rows(users, next_users)
     }
     users
 }
