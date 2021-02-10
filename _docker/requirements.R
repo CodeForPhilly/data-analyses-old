6888dc8632cdd13e90831899b98810a2e7448fd1
Michael Chow <mc_al_github@fastmail.com>
2021-02-10 12:27:24 -0700

    remove more files

--- _docker/requirements.R
+++
@@ -1,4 +0,0 @@
-# has a ton of unnecessary dependencies, can't get one to install
-remotes::install_github("hrbrmstr/slackr")
-remotes::install_version("visNetwork", "2.0.9")
-remotes::install_version("networkD3", "0.4")
